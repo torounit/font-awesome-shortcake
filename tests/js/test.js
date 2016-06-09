f28d0a9d5ae3f55c96c04896a42cbd13188e69c4
Toro_Unit (Hiroshi Urabe) <mail@torounit.com>
2016-06-10 01:35:05 +0900

    refactoring test

--- tests/js/test.js
+++ tests/js/test.js
@@ -7,20 +7,27 @@ const window = jsdom().defaultView;
 const jquery = 'https://code.jquery.com/jquery-1.12.4.js';
 
 describe('FaShortCode', () => {
-	it('should pass when all is ok', (done) => {
+	it('is method jquery depend test', (done) => {
 
 		jsdom.jQueryify(window, jquery, () => {
 			let shortcode = new FaShortCode(window.$);
 
 			describe('replace', () => {
-				it('should return <span class="fa fa-wordpress fa-2x" data-fa-icon="wordpress" data-fa-size="2x"><!-- fa-wordpress --></span>', () => {
-					assert.equal('<span class="fa fa-wordpress fa-2x" data-fa-icon="wordpress" data-fa-size="2x"><!-- fa-wordpress --></span>', shortcode.replace('[fa icon="wordpress" size="2x"]'));
+				it('should return <span>', () => {
+
+					assert.equal(
+						'<span class="fa fa-wordpress fa-2x" data-fa-icon="wordpress" data-fa-size="2x"><!-- fa-wordpress --></span>',
+						shortcode.replace('[fa icon="wordpress" size="2x"]')
+					);
 				});
 			});
 
 			describe('restore', () => {
-				it('should return [fa icon="wordpress" size="2x"]', () => {
-					assert.equal(shortcode.replace('[fa icon="wordpress" size="2x"]'), '<span class="fa fa-wordpress fa-2x" data-fa-icon="wordpress" data-fa-size="2x"><!-- fa-wordpress --></span>');
+				it('should return shortcode', () => {
+					assert.equal(
+						shortcode.replace('[fa icon="wordpress" size="2x"]'),
+						'<span class="fa fa-wordpress fa-2x" data-fa-icon="wordpress" data-fa-size="2x"><!-- fa-wordpress --></span>'
+					);
 				});
 			});
 
