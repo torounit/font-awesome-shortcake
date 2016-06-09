54b6894d61592184ba03e95928bb9025f653bcd7
Toro_Unit (Hiroshi Urabe) <mail@torounit.com>
2016-06-10 01:11:03 +0900

    add test

--- tests/js/test.js
+++ tests/js/test.js
@@ -1,24 +1,33 @@
 import {assert} from 'chai';
 import FaShortCode from '../../src/js/fa';
 
-global.document = require('jsdom').jsdom('<html></html>');
-global.window = document.defaultView;
-var $ = require('jquery')(window);
+import {jsdom} from 'jsdom';
+
+const window = jsdom().defaultView;
+const jquery = 'https://code.jquery.com/jquery-1.12.4.js';
+
+describe('FaShortCode', () => {
+	it('should pass when all is ok', (done) => {
+
+		jsdom.jQueryify(window, jquery, () => {
+			let shortcode = new FaShortCode(window.$);
+
+			describe('replace', () => {
+				it('should return <span class="fa fa-wordpress fa-2x" data-fa-icon="wordpress" data-fa-size="2x"><!-- fa-wordpress --></span>', () => {
+					assert.equal('<span class="fa fa-wordpress fa-2x" data-fa-icon="wordpress" data-fa-size="2x"><!-- fa-wordpress --></span>', shortcode.replace('[fa icon="wordpress" size="2x"]'));
+				});
+			});
+
+			describe('restore', () => {
+				it('should return [fa icon="wordpress" size="2x"]', () => {
+					assert.equal(shortcode.replace('[fa icon="wordpress" size="2x"]'), '<span class="fa fa-wordpress fa-2x" data-fa-icon="wordpress" data-fa-size="2x"><!-- fa-wordpress --></span>');
+				});
+			});
+
+			done();
+		})
+	})
+})
 
 
-describe('FaShortCode', function () {
 
-	let shortcode = new FaShortCode($);
-
-	describe('replace', function () {
-		it('should return <span class="fa fa-wordpress fa-2x" data-fa-icon="wordpress" data-fa-size="2x"></span>', function () {
-			assert.equal('<span class="fa fa-wordpress fa-2x" data-fa-icon="wordpress" data-fa-size="2x"></span>', shortcode.replace('[fa icon="wordpress" size="2x"]'));
-		});
-	});
-
-	describe('restore', function () {
-		it('should return [fa icon="wordpress" size="2x"]', function () {
-			assert.equal(shortcode.replace('[fa icon="wordpress" size="2x"]'), '<span class="fa fa-wordpress fa-2x" data-fa-icon="wordpress" data-fa-size="2x"></span>');
-		});
-	});
-});
